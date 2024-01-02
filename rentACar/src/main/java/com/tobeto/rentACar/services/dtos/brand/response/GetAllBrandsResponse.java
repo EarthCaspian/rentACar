e40c742ad52592ff23be6f3254ee4f56e66f388f
EarthCaspian <hazar.a@outlook.com>
2024-01-02 18:13:56 +0300

    Brands getALL method with dto

---
+++ rentACar/src/main/java/com/tobeto/rentACar/services/dtos/brand/response/GetAllBrandsResponse.java
@@ -0,0 +1,15 @@
+package com.tobeto.rentACar.services.dtos.brand.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetAllBrandsResponse {
+    private String name;
+
+    private String logoPath;
+}

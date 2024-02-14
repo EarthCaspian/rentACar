75eda638e82ac6053dcac4bdeca6d0598b60e20e
EarthCaspian <hazar.a@outlook.com>
2024-02-14 14:30:14 +0300

    missing import fixed

--- rentACar/src/main/java/com/tobeto/rentACar/repositories/RentalRepository.java
+++ rentACar/src/main/java/com/tobeto/rentACar/repositories/RentalRepository.java
@@ -3,6 +3,8 @@ package com.tobeto.rentACar.repositories;
 import com.tobeto.rentACar.entities.concretes.Rental;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RentalRepository extends JpaRepository<Rental, Integer> {
 
 
